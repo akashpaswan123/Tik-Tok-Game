0850ae4f994ab9834aa81a27726298d2405c30cb
Akash <geetadevi9956057@gmail.com>
2024-07-11 18:23:26 +0530

    Tik_Toa_Toe

--- new game/script.js
+++ new game/script.js
@@ -1,9 +1,9 @@
 const boxes = document.querySelectorAll(".box");
 const gameInfo = document.querySelector(".game-info");
 const newGamebtn = document.querySelector(".btn");
-// Decide the current Turn
+
 let currentPlayer;
-// All possible combinations for winning
+
 const winningPostions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -16,7 +16,6 @@ const winningPostions = [
 ];
 let gameGrid;
 
-// Start The Game
 function initGame() {
   currentPlayer = "x";
   gameGrid = ["", "", "", "", "", "", "", "", ""];
@@ -29,8 +28,6 @@ function initGame() {
   gameInfo.innerText = `Current Player - ${currentPlayer.toUpperCase()}`;
 }
 
-
-// Switch player turn on click
 function swapTurn() {
   if (currentPlayer === "x") {
     currentPlayer = "o";
@@ -40,9 +37,8 @@ function swapTurn() {
   gameInfo.innerText = `Current Player - ${currentPlayer.toUpperCase()}`;
 }
 
-// Event listener
+
 function handleClick(index) {
-  // Make sure only, Empty cells are filled
   if (gameGrid[index] === "") {
     boxes[index].style.pointerEvents = "none";
     boxes[index].innerHTML = currentPlayer.toUpperCase();
@@ -52,7 +48,6 @@ function handleClick(index) {
   }
 }
 
-// Check if game is over
 function checkGameOver() {
   let result = "";
   winningPostions.forEach((position) => {
@@ -73,7 +68,7 @@ function checkGameOver() {
       boxes[position[2]].classList.add("win");
     }
   });
-  //  We Have A Winner
+
   if (result !== "") {
     gameInfo.innerText = `Winner Player - ${result}`;
     newGamebtn.classList.add("active");
@@ -83,7 +78,7 @@ function checkGameOver() {
   gameGrid.forEach((box) => {
     if (box === "") boardFilled = false;
   });
-  // Board is filled, but game is tie
+ 
   if (boardFilled) {
     gameInfo.innerText = "Game Tied !";
     newGamebtn.classList.add("active");
@@ -91,7 +86,7 @@ function checkGameOver() {
   }
 }
 
-// adding event listeners
+
 boxes.forEach((box, index) => {
   box.addEventListener("click", () => {
     handleClick(index);
